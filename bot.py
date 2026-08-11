f2455b170515856d906b3eb1fee339cafee08497
naitikjangid0-beep <naitikjangid0@gmail.com>
2026-08-11 23:03:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,22 @@
+import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running live!"
+
+def run():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
 import random
 import sqlite3
 import telebot
